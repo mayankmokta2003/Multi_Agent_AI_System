d674c10fe5d0a1eab6b20fbf00648710da5c500e
Mayank Mokta <mayankmokta@gmail.com>
2026-05-11 14:54:50 +0530

    Multi agent ai system

---
+++ tools.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.tools import tool
+import requests 
+import os
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+from rich import print
+
+
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def web_search(query : str) -> str:
+    """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
+    results = tavily.search(query = query, max_results = 5)
+    output = []
+    for r in results["results"]:
+        output.appent(
+            f"Title: {r["title"]} \nURL: {r["url"]} \nSnippet: {r["content"][:300]} \n"
+        )
+    
+
+
+# print(web_search.invoke("news of west bengal elections?"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
